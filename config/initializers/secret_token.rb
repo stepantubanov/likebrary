c55aff9c318779d498a07e0c12f635eb86bf1ac2
Stepan Tubanov <stepan773@gmail.com>
2013-05-21 03:20:12 +0900

    Rails with README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Likebrary::Application.config.secret_token = '9821fba1997515806b78cb68b7b8f93439d3db8f9cb895e9f6983dd8efd73ed08f4b2468b71fc31fe14bc4588027709a010fd13129ec832c580ca03129f822d9'
